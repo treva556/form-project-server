150f72ce274cfe897f410102ef9b4cc9c9e333df
treva556 <marktreva92@gmail.com>
2023-06-15 03:14:50 +0300

    timeout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Other routes in your application
+
   resources :users, only: [:index, :create]
 
   get '/users/new', to: 'users#new'
   post '/users', to: 'users#create'
+  get '/users/fetch_all_data', to: 'users#fetch_all_data'
+
 end
